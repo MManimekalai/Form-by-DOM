9bdd9c20c5a0583487ae39b8da97fce728be494b
MManimekalai <124970075+MManimekalai@users.noreply.github.com>
2023-04-12 11:06:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo() {
+    let firstName = document.getElementById("firstname").value;
+    let middleName = document.getElementById("middlename").value;
+    let lastName = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let mobile = document.getElementById("mobile").value;
+  
+    console.log("First Name: " + firstName);
+    console.log("Middle Name: " + middleName);
+    console.log("Last Name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Mobile: " + mobile);
+  }
+  
